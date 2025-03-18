2483ac79ee0d3b14b5c0494500af7a59eed5cc2f
Mahi singh <aka.mahisingh@gmail.com>
2025-03-18 15:15:19 +0530

    bug fixes

--- models/StaffVarification.js
+++ models/StaffVarification.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const staffSchema = new mongoose.Schema({
   Name: String,
-  StaffID: { type: Number, required: true, unique: true },
+  StaffID: { type: Number },
   JoinningData: Date,
   Designation: String,
   DOB: { type: Date, required: true },
@@ -12,6 +12,6 @@ const staffSchema = new mongoose.Schema({
   Address: String,
 });
 
-const Staff = mongoose.model('Staff', staffSchema);
+const Staff = mongoose.model('Staff Data', staffSchema);
 
 export default Staff;
