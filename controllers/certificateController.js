6e5a214d74fdf0b409b18264a59c48e61c1fe0a4
Mahi singh <aka.mahisingh@gmail.com>
2025-04-08 09:53:16 +0530

    final fixes

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import User from '../models/onlineCourseRegister.js';
+import User from '../models/register.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
