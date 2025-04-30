fcb2752e821be7c99c07ccb8f9d6a69834bbf460
Mahi singh <aka.mahisingh@gmail.com>
2025-04-30 14:25:52 +0530

    Update Subject_code.js

--- utils/Subject_code.js
+++ utils/Subject_code.js
@@ -1,27 +1,27 @@
 const CERTIFICATION_IN_COMPUTER_APPLICATION = {
     "CS-01": "Basic Computer",
     "CS-02": "Windows Application: MS Office",
-    "CS-03": "Operaing System",
+    "CS-03": "Operating System",
     "CS-04": "Web Publisher: Internet Browsing"
 };
 
 const DIPLOMA_IN_COMPUTER_APPLICATION = {
     "CS-01": "Basic Computer",
     "CS-02": "Windows Application: MS Office",
-    "CS-03": "Operaing System",
+    "CS-03": "Operating System",
     "CS-04": "Web Publisher: Internet Browsing",
-    "CS-05": "COMPUTER ACCOUTENCY:  TALLY"
+    "CS-05": "Computer Accountancy: Tally"
 };
 
 const ADVANCE_DIPLOMA_IN_COMPUTER_APPLICATION = {
     "CS-01": "Basic Computer",
     "CS-02": "Windows Application: MS Office",
-    "CS-03": "Operaing System",
-    "CS-05": "COMPUTER ACCOUTENCY:  TALLY",
-    "CS-06": "DESKTOP PUBLISHING: PHOTOSHOP"
+    "CS-03": "Operating System",
+    "CS-05": "Computer Accountancy: Tally",
+    "CS-06": "Desktop Publishing: Photoshop"
 };
 
-const CERTIFICATION_IN_COMPUTER_ACCOUTENCY = {
+const CERTIFICATION_IN_COMPUTER_ACCOUNTANCY = {
     "CS-01": "Basic Computer",
     "CS-02": "Windows Application: MS Office",
     "CS-07": "Computerized Accounting With Tally",
@@ -33,7 +33,7 @@ const DIPLOMA_IN_COMPUTER_ACCOUNTANCY = {
     "CS-02": "Windows Application: MS Office",
     "CS-07": "Computerized Accounting With Tally",
     "CS-08": "Manual Accounting",
-    "CS-09": "Tally ERP 9 & Tally Prime",
+    "CS-09": "Tally ERP 9 & Tally Prime"
 };
 
 
