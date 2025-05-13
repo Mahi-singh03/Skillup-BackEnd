f9ab29c501fe3690a0584626886e5f667ad7b110
Mahi singh <aka.mahisingh@gmail.com>
2025-05-13 16:05:10 +0530

    everything worlks

--- routes/feeRoutes.js
+++ routes/feeRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getStudentDetails, updateStudentFees, getAllStudentFees } from '../controllers/fees.js';
+import { getAllStudentFees, updateStudentFees, getAllStudentFees } from '../controllers/fees.js';
 
 const router = express.Router();
 
-router.get('/student', getStudentDetails);
+router.get('/student', getAllStudentFees);
 router.put('/update', updateStudentFees);
 router.get('/getAll', getAllStudentFees);
 
